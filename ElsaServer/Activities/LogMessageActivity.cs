f774a87a25772497595e13ff9d4dd10d543dd84a
AmirHome <amir.email@yahoo.com>
2025-05-29 16:02:24 +0300

    Add LogMessageActivity to log custom messages to the console

---
+++ ElsaServer/Activities/LogMessageActivity.cs
@@ -0,0 +1,26 @@
+using Elsa.Workflows;
+using Elsa.Workflows.Attributes;
+using Elsa.Workflows.Models;
+using Elsa.Workflows.Activities;
+using Elsa.Expressions;
+using Microsoft.Extensions.Logging;
+using Elsa.Extensions;
+
+namespace ElsaServer.Activities
+{
+    [Activity("Logging", "Log Custom Message", "Logs a custom message to the console")]
+    public class LogMessageActivity : CodeActivity
+    {
+        public Input<string> Message { get; set; } = default!;
+
+        protected override void Execute(ActivityExecutionContext context)
+        {
+            var logger = context.GetRequiredService<ILogger<LogMessageActivity>>();
+            var messageString = Message.Get(context);
+            if (!string.IsNullOrWhiteSpace(messageString))
+                logger.LogInformation($"Logger:\"{messageString}\"");
+            else
+                logger.LogWarning("LogMessageActivity: No message provided.");
+        }
+    }
+}
