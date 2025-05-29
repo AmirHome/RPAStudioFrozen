8179116dc017aa5350950777b0fee57e4ca2aac3
AmirHome <amir.email@yahoo.com>
2025-05-29 17:27:02 +0300

    Add If activity and IfWorkflow to implement conditional branching in workflows

---
+++ ElsaServer/Activities/If.cs
@@ -0,0 +1,27 @@
+using Elsa.Workflows;
+using Elsa.Workflows.Models;
+using Elsa.Workflows.Attributes;
+
+namespace ElsaServer.Activities;
+
+[Activity("If", "AmirHoss", "Executes one of two branches based on a condition.")]
+
+public class If : Activity
+{
+
+    public Input<bool> Condition { get; set; } = default!;
+    public IActivity? Then { get; set; }
+    public IActivity? Else { get; set; }
+
+    protected override async ValueTask ExecuteAsync(ActivityExecutionContext context)
+    {
+        var result = context.Get(Condition);
+        var nextActivity = result ? Then : Else;
+        await context.ScheduleActivityAsync(nextActivity, OnChildCompleted);
+    }
+
+    private async ValueTask OnChildCompleted(ActivityCompletedContext context)
+    {
+        await context.CompleteActivityAsync();
+    }
+}
---
+++ ElsaServer/Activities/IfWorkflow.cs
@@ -0,0 +1,17 @@
+using Elsa.Workflows;
+using Elsa.Workflows.Activities;
+
+namespace ElsaServer.Activities;
+
+public class IfWorkflow : WorkflowBase
+{
+    protected override void Build(IWorkflowBuilder builder)
+    {
+        builder.Root = new If
+        {
+            Condition = new(context => DateTime.Now.IsDaylightSavingTime()),
+            Then = new WriteLine("Hello to the light side!"),
+            Else = new WriteLine("Hello to the dark side!")
+        };
+    }
+}
